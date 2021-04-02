63a198606ddd58a81df43ddc996c675380bf197d
svn5969 <sourav.svn.10@gmail.com>
2021-04-02 22:12:25 +0600

    fix link issue

--- src/Components/Headers/Headers.js
+++ src/Components/Headers/Headers.js
@@ -3,29 +3,36 @@ import { Nav, Navbar } from 'react-bootstrap';
 import { UserContext } from '../../App';
 import logo from '../../images/all in one vv.png'
 
-const Header = () => {
-    const [loggedInUser, setLoggedInUser] =useContext(UserContext);
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
-console.log(loggedInUser);
+const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    console.log(loggedInUser);
 
     return (
         <>
 
             <Navbar collapseOnSelect expand="lg" bg="muted" variant="primary">
-                <Navbar.Brand ><img style={{height:'50px',width:'70px',marginLeft:'60px'}} src={logo}></img></Navbar.Brand>
-                <Navbar.Toggle style={{ backgroundColor: "skyblue"}}aria-controls="responsive-navbar-nav" />
+                <Navbar.Brand ><img style={{ height: '50px', width: '70px', marginLeft: '60px' }} src={logo}></img></Navbar.Brand>
+                <Navbar.Toggle style={{ backgroundColor: "skyblue" }} aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
                     </Nav>
                     <Nav>
-                        <Nav.Link href="/home">Home</Nav.Link>
-                        <Nav.Link href="/addProduct">Admin</Nav.Link>
-                        <Nav.Link href="/orders">Order</Nav.Link>
-                        <Nav.Link href="/signin">
-                        {loggedInUser.userName ?loggedInUser.userName:<button className="btn btn-primary btn-sm">Log In</button>}
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/addProduct">Admin</Nav.Link>
+                        <Nav.Link as={Link} to="/orders">Order</Nav.Link>
+                        <Nav.Link as={Link} to="/signin">
+                            {loggedInUser.userName ? loggedInUser.userName : <button className="btn btn-primary btn-sm">Log In</button>}
 
 
-      </Nav.Link>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,28 +3,29 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Product from '../Product/Product';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import './Home.css'
 
 const Home = () => {
-    const [products,setProducts] = useState([])
-    
-   
+    const [products, setProducts] = useState([])
 
-    useEffect(()=>{
-           fetch('https://infinite-shelf-56932.herokuapp.com/products')
-           .then(res=>res.json())
-           .then(data=>setProducts(data))
-    },[])
+
+
+    useEffect(() => {
+        fetch('https://infinite-shelf-56932.herokuapp.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div className="row">
-        <div className="d-flex">
+            <div className="d-flex spin">
 
-        {
-                products.length === 0 && <CircularProgress color="secondary" />
-            }
-        </div>
-          
+                {
+                    products.length === 0 && <CircularProgress color="secondary" />
+                }
+            </div>
+
             {
-                products.map(product =><Product  product={product} ></Product>)
+                products.map(product => <Product product={product} ></Product>)
             }
         </div>
     );
